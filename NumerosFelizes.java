e31550c878856c4562782fdfd8a284015736ca12
Bruno Welsing <brunowelsing@yahoo.com.br>
2014-09-02 17:29:24 -0300

    com o método eFeliz

--- NumerosFelizes.java
+++ NumerosFelizes.java
@@ -12,17 +12,36 @@ class NumerosFelizes {
 		return numDividido;
 	}
 
+	public int somaDosAlg(int [] numDividido){
+		int soma = 0;
+		for (int i = 0; i < numDividido.length ; i++){
+			soma = soma + numDividido[i] * numDividido[i]; 
+		}
+		return soma;
+	}
 
 	public boolean eFeliz(int numInserido){
-		int[] numDividido = DividirNum(numInserido);
-			int soma = 0;
-		while(soma != 1 && soma != numInserido){
-			soma = 0;
-			for (int i = 0; i < numDividido.length ; i++){
-				soma = soma + numDividido[i] * numDividido[i]; 
-			}
-			numDividido = DividirNum(soma);
+		int numDividido = DividirNum(numInserido);
+		int soma = 0;
+		int i = 0;
 
+	List<Integer> ant = new ArrayList<Integer>();
+		public void add(int num)
+ 			{
+  				this.ant.add(num);
+  			}
+		
+		while(soma != 1 ){
+			soma = somaDosAlg(numDividido);
+			ant.add(soma);
+			numDividido = DividirNum(soma);
+			i++;
+			for(j = 0; j < ant.length(); j++){
+				if (soma = ant[j]){
+					return 0;
+				}
+
+			}
 		}
 		System.out.println(soma);
 		return true;
