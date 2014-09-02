5c473ca58b9d79a1b7a68da06b12adb566c5a240
Ronnie Paskin <ronnie@megaputz.com>
2014-09-02 17:07:36 -0300

    Adiciona testes requeridos pelo enunciado

--- NumerosFelizesTestDrive.java
+++ NumerosFelizesTestDrive.java
@@ -6,6 +6,13 @@ public class NumerosFelizesTestDrive {
       NumerosFelizes teste = new NumerosFelizes();
 //      System.out.println("\nTrue:");
         System.out.println(Arrays.toString(teste.DividirNum(2034)));
+
+        // Testes requeridos pelo enunciado:
+        System.out.println("Dividindo 1: ");
+        System.out.println(Arrays.toString(teste.DividirNum(1)));
+        System.out.println("Dividindo 123: ");
+        System.out.println(Arrays.toString(teste.DividirNum(123)));
+
 //      System.out.println("\nTrue:");
 //      System.out.println(teste.eFeliz(13));
 //      System.out.println("\nFalse:");
