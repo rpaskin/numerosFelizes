4b3f8fa6e9296813e8e91bdfd9fe47e4435708a0
Bruno Welsing <brunowelsing@yahoo.com.br>
2014-09-02 15:17:35 -0300

    Primeira mudan�a, split adicionado

--- NumerosFelizes.java
+++ NumerosFelizes.java
@@ -1,12 +1,18 @@
 import java.util.*;
 
 class NumerosFelizes {
-  public boolean eFeliz(int args){
-    if (args ==  13){
-      return true;
-    }
-    else{
-      return false;
-    }
-  }
+ 	public int[] DividirNum(int args){
+  		int i;
+  		int numInserido = args;
+		String[] numDividido = numInserido.split(""); 
+  		int comprimentoNumInserido = String.valueOf(numInserido).length();
+  		System.out.println(comprimentoNumInserido);
+  		int numDividido[] = new int[comprimentoNumInserido];
+		for(i = 0; i < comprimentoNumInserido; i++){
+    		numDividido[i] = numInserido % 10;
+    		System.out.print(numDividido[i] + " ");
+    		numInserido = numInserido / 10;
+		}
+		return numDividido;
+	}
 }
--- NumerosFelizesTestDrive.java
+++ NumerosFelizesTestDrive.java
@@ -2,12 +2,14 @@ public class NumerosFelizesTestDrive {
     public static void main(String[] args)
     {
       NumerosFelizes teste = new NumerosFelizes();
-      System.out.println("\nTrue:");
-      System.out.println(teste.eFeliz(13));
-      System.out.println("\nFalse:");
-      System.out.println(teste.eFeliz(3));
-      System.out.println("\nTrue:");
-      System.out.println(teste.eFeliz(10));
-
+//      System.out.println("\nTrue:");
+      System.out.println(teste.DividirNum(2034));
+//      System.out.println("\nTrue:");
+//      System.out.println(teste.eFeliz(13));
+//      System.out.println("\nFalse:");
+//      System.out.println(teste.eFeliz(3));
+//      System.out.println("\nTrue:");
+//      System.out.println(teste.eFeliz(10));
+//      teste.eFeliz(48);
     }
 }
